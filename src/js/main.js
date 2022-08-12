353e84de1df53280a5bc91576e6f335a19c664b7
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2022-08-12 11:28:54 -0300

    :sparkles: feat: Skill carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
-const headerMain = document.querySelector('header.h-main')
+const headerMain = document.querySelector('header.h-main');
+const skillsList = document.querySelector('.carousel .skills__list');
+const skills = document.querySelectorAll('.skills__list .skill__item');
+const arrowLeft = document.querySelector('.skills__carousel .left span');
+const arrowRight = document.querySelector('.skills__carousel .right span');
+let indexX = 0;
+
+const goingLeft = () => {
+  if(indexX > 0) {
+    indexX--;
+    skillsList.style.transform = `translateX(${(-indexX * 18)}rem)`;
+  }
+}
+
+const goingRight = () => {
+  if(indexX < skills.length - 1) {
+    indexX++;
+    skillsList.style.transform = `translateX(${(-indexX * 18)}rem)`;
+  }
+}
 
 document.addEventListener("scroll", function() {
   let positionY = window.pageYOffset;
@@ -8,3 +27,14 @@ document.addEventListener("scroll", function() {
     headerMain.classList.remove('h-main--highlighted')
   }
 });
+
+arrowLeft.addEventListener('click', () => { goingLeft() });
+arrowRight.addEventListener('click', () => { goingRight() });
+
+setInterval(() => {
+  indexX++;
+  if(indexX >= skills.length - 1) {
+    indexX = 0;
+  }
+  skillsList.style.transform = `translateX(${-indexX * 18}rem)`;
+}, 3000);
